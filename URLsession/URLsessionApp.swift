ce71a75d7f6f15c89258c005f520a2aa0e42df3e
GrimProg <vavilovila01@gmail.com>
2026-05-02 16:53:04 +0600

    Initial Commit

---
+++ URLsession/URLsessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLsessionApp.swift
+//  URLsession
+//
+//  Created by Вавилов Илья on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct URLsessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ URLsessionUITests/URLsessionUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  URLsessionUITestsLaunchTests.swift
+//  URLsessionUITests
+//
+//  Created by Вавилов Илья on 2/5/26.
+//
+
+import XCTest
+
+final class URLsessionUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
